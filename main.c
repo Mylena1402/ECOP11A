4df8af1e781bf4a667ca070e4a8480adcf178696
Mylena1402 <d2024013705@unifei.edu.br>
2024-10-22 12:23:22 +0000

    Primeira versão

---
+++ main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+void handle_client(int client_socket) {
+    char buffer[BUFFER_SIZE];
+    int bytes_read = read(client_socket, buffer, BUFFER_SIZE - 1);
+    if (bytes_read < 0) {
+        perror("read");
+        close(client_socket);
+        return;
+    }
+
+    buffer[bytes_read] = '\0';
+    printf("Received request:\n%s\n", buffer);
+
+    const char *response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html; charset=UTF-8\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "<!DOCTYPE html>"
+        "<html>"
+        "<head><title>Servidor Mylena</title></head>"
+        "<body><h1>Controlador de Acesso</h1>"
+        "<p>Olá, seja bem-vindo ao controlador de acesso!</p>"
+        "<p>Para acessar o sistema, clique <a href='http://localhost:8080/login'>aqui</a>.</p>"
+        "</body>"
+        "</html>";
+        
+    write(client_socket, response, strlen(response));
+    close(client_socket);
+}
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, 10) < 0) {
+        perror("listen");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server is listening on port %d\n", PORT);
+
+    while (1) {
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_socket < 0) {
+            perror("accept");
+            continue;
+        }
+
+        handle_client(client_socket);
+    }
+
+    close(server_socket);
+    return 0;
+}
